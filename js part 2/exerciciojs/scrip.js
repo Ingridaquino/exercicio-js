33519686f07f30da389b43ce1746723b52de0bfa
Ingrid Aquino <ingridaquinosoares@live.com>
2021-11-05 18:12:16 -0300

    condicional

---
+++ js part 2/exerciciojs/scrip.js
@@ -0,0 +1,20 @@
+// Crie uma função que calcule a média de 4 notas e exibe o resultado no console.log
+function mediaTotal(num1 = 5, num2 = 8){
+    let media = 2;
+
+    return (num1 + num2) / media;
+    
+    
+}
+
+console.log(mediaTotal());
+
+//Crie uma função que faça a conversão de Celsius para Fahrenheit e exiba o resultado no console.log 
+
+const celsius =(cel = 23) =>{
+    return cel * 9/5 + 32;
+
+    
+}
+
+console.log(celsius());
